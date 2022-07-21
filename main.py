fce7182fee5dc18d4d78831efee3a76b8bbfb9c6
Stefan Agudaru <agudarustefan@gmail.com>
2022-07-21 18:59:06 +0300

    wwww

---
+++ main.py
@@ -0,0 +1,18 @@
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
+from django.urls import path
+from django.conf import settings
+
+settings.configure( DEBUG=True, ROOT_URLCONF='__name__', SECRET_KEY='qwer')
+
+def handle_request(request):
+    return HttpResponse("hello, Django")
+
+urlpatterns = [
+    path('hello', handle_request),
+    ]
+
+
+
+if __name__ == '__main__':
+    execute_from_command_line()
